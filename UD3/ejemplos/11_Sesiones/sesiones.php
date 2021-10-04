cce73f51478fb599aeeb05a12b85ce4a84a17b18
esgrao <ester.grao@gmail.com>
2021-10-04 19:29:21 +0100

    UD3

--- UD3/ejemplos/11_Sesiones/sesiones.php
+++ UD3/ejemplos/11_Sesiones/sesiones.php
@@ -3,7 +3,20 @@
 /*
 * PHP es un protocolo son estado -> las diferentes peticiones son independientes
 *   -> para asociarlas se usan las sesiones
-* Al iniciar una sesión el servidor asigna y envía al usuario un identificador de sesión
-*   en la siguientes peticiones el cliente envía al usuario un identificador de sesión
-* Para controlar la sesión el servidor deja una cookie con el id de sesión en el cliente
-*
+* Al iniciar una sesión el servidor:
+*   -> asigna y envía al usuario un identificador de sesión
+*   -> en las siguientes peticiones el usuario envía al servidor ese identificador de sesión
+*   -> para controlar la sesión el servidor deja una cookie con el id de sesión en el cliente
+*/
+
+// Crear sesión: start_session()
+// Una vez creada, se usa la variable global $_SESSION para compartir información entre los scripts
+
+session_start();
+if ( !isset ($_SESSION['count'])) {
+    $_SESSION['count'] = 0;
+} else {
+    $_SESSION['count']++;
+}
+echo "hola".$_SESSION['count'];
+echo "<br><a href='sesiones_uso_basico2.php'>Siguiente</a>";
