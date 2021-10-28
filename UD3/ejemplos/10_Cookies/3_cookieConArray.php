6aa460da3a5575b3336a072d3a95bf085ab9a07a
esgrao <ester.grao@gmail.com>
2021-10-28 17:11:50 +0100

    UD3

--- UD3/ejemplos/10_Cookies/3_cookieConArray.php
+++ UD3/ejemplos/10_Cookies/3_cookieConArray.php
@@ -5,7 +5,12 @@ setcookie("user", "Fulanito de Tal", time() + 3600);
 //ojo para guardar arrays:
 $hobbies = ['futbol', 'música rock', 'tocar la guitarra con mis amigos'];
 setcookie("hobbies", serialize($hobbies), time() + 3600);
+$hobbies = unserialize($_COOKIE["hobbies"]);
+
 setcookie("hobbies2", json_encode($hobbies), time() + 3600);
+$hobbies = json_decode($_COOKIE["hobbies"]);
+
+
 
 //y objetos:
 $persona = new Persona("Juan", "Pérez", 21);
