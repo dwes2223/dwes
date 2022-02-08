2922368e8d1662a49bf5cf4b248257ad48bddec4
esgrao <ester.grao@gmail.com>
2022-02-08 16:09:52 +0000

    ud5

--- UD5/clase07/public/js/ejemploajax.js
+++ UD5/clase07/public/js/ejemploajax.js
@@ -6,9 +6,30 @@ $('#formulario').click(function (e) {
     data = $('#filtro').val();
     console.log(data);
 
-    $.get("/studies/filter?filter="+data, function(data, status){
+    $.get("/studies/filter?filter="+data, function(dataJSON, status){
         // console.log("Data: " + data + "\nStatus: " + status);
-        console.log(data);
+        console.log(dataJSON);
       //$('#destinofiltro').html(data);
     });      
 })
+
+
+//Si queremos enviar post en ajax hay que añadir los datos.
+// (https://www.w3schools.com/JQuery/jquery_ajax_get_post.asp)
+//$("button").click(function() {
+    //   $.post("demo_test_post.asp",
+    //   {
+    //     name: "Donald Duck",
+    //     city: "Duckburg"
+    //   },
+    //   function(data, status){
+    //     alert("Data: " + data + "\nStatus: " + status);
+    //   });
+//}); 
+    
+    // Si eso viene de un formulario podemos leer el 
+    // formulario de una tacada con serialize de jquery
+    // const data = $('#miFormulario').serialize()
+    // importante el data debe incluir el csrf si no devolverá
+    // error 419 
+    //})
